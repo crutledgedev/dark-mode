addf23e6a0c1ab98ef6910dc02cce8b0be0b3116
Chance Rutledge <chancerutledge2@gmail.com>
2019-12-04 18:52:52 -0800

    mvp commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
+import {useDarkMode} from '../hooks/useDarkMode'
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
-  const toggleMode = e => {
-    e.preventDefault();
-    setDarkMode(!darkMode);
-  };
+  const [darkMode, setToggleMode] = useDarkMode(false);
+  // const toggleMode = e => {
+  //   e.preventDefault();
+  //   setDarkMode(!darkMode);
+  // };
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
       <div className="dark-mode__toggle">
         <div
-          onClick={toggleMode}
+          onClick={setToggleMode}
           className={darkMode ? 'toggle toggled' : 'toggle'}
         />
       </div>
